08cd1f521e0dab2d725853ecfd8aa11d84e2346c
Mekdes Menji <mekdesmenji79@gmail.com>
2025-06-12 20:36:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const removeBtn = document.createElement('button');
         removeBtn.textContent = 'Remove';
-        removeBtn.className = 'remove-btn';
+        removeBtn.classList.add('remove-btn');
 
         removeBtn.onclick = () => {
             taskList.removeChild(li);
